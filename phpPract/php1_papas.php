f2a12d5abd60e4e6947901b60d6fdaa96b0f26b0
mebzmoren <ellyzajpapas@su.edu.ph>
2023-03-10 16:11:45 +0800

    seatwork

--- phpPract/php1_papas.php
+++ phpPract/php1_papas.php
@@ -8,10 +8,40 @@
 </head>
 <body>
     <?php
-        $arr = array ("coffeebean" => "coffee", "sansrival" => "cakes", "stateresa" => "filipinofood", "2story" => "koreanfood", "mooon" => "mexicanfood");
+        $count1 = 1;
+        $restaurants = array (  "coffeebean" => "coffee", 
+                        "sansrival" => "cakes",
+                        "stateresa" => "filipinofood",
+                        "2story" => "koreanfood",
+                        "mooon" => "mexicanfood");
+        echo "Restaurants of Dumaguete and their specialties:<br>";
 
-        foreach($arr as $place => $food){
-            echo $place . "has" . $food;
+        //Create a foreach loop to access each element.
+        foreach($restaurants as $place => $food){
+            echo $count1. ". " . $place . " has " . $food . "<br>" ;
+            $count1++;
+        }
+
+        echo "<br>";
+
+        //Pass every element to a function and display the value there.
+        $count2 = 1;
+        function display($key, $value){
+            echo $key . "has a value of " . $value . "<br>";
+        }
+
+        foreach($restaurants as $place => $food){
+            display($food, $count2);
+            $count2++;
+        }
+
+        echo "<br>";
+
+        //Search for the string “koreanfood” using strcmp function. Prompt a message if a match is found.
+        foreach($restaurants as $place => $food){
+            if(strcmp($food, "koreanfood") == 0){
+                echo "The food " . $food . " exists.";
+            }
         }
     ?>
 </body>
